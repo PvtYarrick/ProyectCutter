207b98adc5d952e35c2ed14b4e694c1827cc1a35
DESKTOP-FCB35RO\Yarrick <regueiromag@gmail.com>
2018-01-05 00:38:49 +0100

    Added a choppy animator that probably needs a bit more work but I´m dying

---
+++ Proyect Cutter/Assets/Scripts/ShipAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAnimator : MonoBehaviour {
+
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Rotating", move);
+		
+	}
+}
