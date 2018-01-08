9872df88748adb310c140aa8a43a25d45cfdc55a
DESKTOP-FCB35RO\Yarrick <regueiromag@gmail.com>
2018-01-08 01:10:25 +0100

    Added keyboard support for the menu and funcionality framework, plus some animations (Commited without Library, may The Ancestor protect us)

---
+++ Proyect Cutter/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject rocket;
+
+    public void GoToScene(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void GoToLevel(string level)
+    {
+        StartCoroutine(LaunchRocket(level));
+    }
+
+    IEnumerator LaunchRocket(string level)
+    {
+        Animator anim = rocket.GetComponent<Animator>();
+        anim.enabled = true;
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(level);
+    }
+
+   
+
+
+
+
+
+}
